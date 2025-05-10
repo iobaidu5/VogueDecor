6016bba84c6d8ff336565794189cb12571b0af9a
iobaidu5 <i.obaidu5@gmail.com>
2025-05-11 02:12:49 +0500

    updated

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,2 +1,16 @@
-import { handlers } from 'auth';
-export const { GET, POST } = handlers;
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID! || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET! || "",
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+});
+
+export { handler as GET, handler as POST }; // for App Router
+// OR
+export default handler; // for Pages Router
