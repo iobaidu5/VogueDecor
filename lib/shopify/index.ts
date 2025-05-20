9c94b4cf8b91831e70232e6b28f92c7673c110a1
iobaidu5 <i.obaidu5@gmail.com>
2025-05-20 22:12:44 +0500

    no store

--- lib/shopify/index.ts
+++ lib/shopify/index.ts
@@ -66,7 +66,7 @@ type ExtractVariables<T> = T extends { variables: object }
   ? T["variables"]
   : never;
 export async function shopifyFetch<T>({
-  cache = "force-cache",
+  cache = "no-store",
   headers,
   query,
   tags,
