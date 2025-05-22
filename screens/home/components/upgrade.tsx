2a4919636f7e0a1d85479cd9f41dad7c5ce9dd9d
iobaidu5 <i.obaidu5@gmail.com>
2025-05-23 00:31:21 +0500

    added footer faq

--- screens/home/components/upgrade.tsx
+++ screens/home/components/upgrade.tsx
@@ -1,30 +1,33 @@
-import overlayImage from 'media/png/overlayImage.png';
-import upgradeImage from 'media/png/upgradeImage.png';
-import logobtn from 'media/svg/logobtn.svg';
-import Image from 'next/image';
+import Image from "next/image";
+import overlayImage from "media/png/overlayImage.png";
+import upgradeImage from "media/png/upgradeImage.png";
+import logobtn from "media/svg/logobtn.svg";
 
 const Upgrade = () => {
   return (
-    <div className="relative h-[30vh] w-full bg-[#013540]">
-      <div className="absolute inset-0 flex flex-col justify-center">
-        <div className="container mx-auto px-40 md:px-40">
-          <div className="flex flex-col md:flex-row items-center justify-between w-full">
+    <div className="relative w-full h-[25vh] sm:h-[20vh] md:h-[25vh] lg:h-[25vh] bg-[#013540]">
+      {/* Optional: overlay image */}
+      {/* <Image src={overlayImage} alt="overlay" className="absolute inset-0 object-cover w-full h-full opacity-10" /> */}
+
+      <div className="absolute inset-0 flex items-center justify-center">
+        <div className="w-full px-4 sm:px-6 md:px-10 lg:px-40">
+          <div className="flex flex-col md:flex-row items-center justify-between w-full space-y-6 md:space-y-0">
             {/* Left Side - Text */}
-            <div className="text-white text-center md:text-left mb-6 md:mb-0">
-              <h1 className="mb-2 font-medium font-poppins xs:text-[24px] md:text-[42px] text-[#05E5C8]">
+            <div className="text-white text-center md:text-left">
+              <h1 className="mb-2 font-medium font-poppins text-[28px] sm:text-[32px] md:text-[38px] lg:text-[42px] text-[#05E5C8]">
                 Upgrade with Ease
               </h1>
-              <p className="xs:text-[16px] md:text-[18px] font-poppins ml-1 tracking-wide">
+              <p className="text-[16px] sm:text-[17px] md:text-[18px] font-poppins tracking-wide">
                 Redefine your space hassle free with
               </p>
             </div>
 
             {/* Right Side - Button Group */}
             <div className="flex items-center">
-              <button className="flex items-center justify-center rounded-l-lg bg-white xs:p-1 md:p-2">
-                <Image src={logobtn} alt="logo" className="xs:w-[90px] md:w-auto" />
+              <button className="flex items-center justify-center rounded-l-lg bg-white px-3 py-2 sm:py-2">
+                <Image src={logobtn} alt="logo" className="w-[90px] md:w-auto" />
               </button>
-              <button className="rounded-r-lg bg-[#05E5C8] h-[59px] py-1 xs:px-4 md:px-6 text-black font-medium">
+              <button className="rounded-r-lg bg-[#05E5C8] h-[52px] px-4 sm:px-5 md:px-6 text-black font-medium">
                 Apply Now
               </button>
             </div>
