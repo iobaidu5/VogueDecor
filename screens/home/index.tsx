f93eff5cfe0cc3a8289762e214d8c8b6b6231b22
iobaidu5 <i.obaidu5@gmail.com>
2025-06-03 20:34:27 +0500

    fix btn

--- screens/home/index.tsx
+++ screens/home/index.tsx
@@ -61,7 +61,7 @@ export default function MainPage() {
             className={isMobile ? "object-contain mt-12 h-auto" : "object-cover"}
             priority
           />
-          <div className="absolute bottom-[65%] left-1/2 transform -translate-x-1/2">
+          <div className="absolute bottom-[65%] md:bottom-[5%] left-1/2 transform -translate-x-1/2">
             <Link
               href="/search/outdoor-furniture"
               className="bg-black  text-white px-4 md:px-6 py-3 text-sm md:text-md rounded hover:bg-gray-900 transition duration-300"
