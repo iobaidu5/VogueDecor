2612e5b0511fc08f0f530105d81298e5ac5ff239
“iobaidu5” <i.obaidu5@@gmail.com>
2025-07-04 02:33:05 +0500

    sale remv

--- screens/home/index.tsx
+++ screens/home/index.tsx
@@ -46,9 +46,9 @@ export default function MainPage() {
     <>
       <div className="w-full overflow-x-hidden">
         <section className={isMobile ? "mt-12 relative w-full h-auto" : "relative w-full h-screen overflow-hidden"}>
-          <div className='relative z-[1000000000]'>
+          {/* <div className='relative z-[1000000000]'>
             <SaleBar />
-          </div>
+          </div> */}
           <Image
             src={
               i18n.language === 'fr'
